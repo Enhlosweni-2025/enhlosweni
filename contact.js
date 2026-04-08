8deb4d4b2aac52a6df2d0fa4787c7c0123f25122
Enhlosweni Enterprise <inhlosoenhle5@gmail.com>
2026-04-08 21:06:51 +0200

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,12 @@
+contact.js
+
+const faqQuestions = document.querySelectorAll(".faq-question");
+
+faqQuestions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const faqItem = question.parentElement;
+
+    // Toggle active state
+    faqItem.classList.toggle("active");
+  });
+});
